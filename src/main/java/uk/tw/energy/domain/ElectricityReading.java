f4e44c1a3736ffce4fe7a9ef59217b904362ad0b
Ayush <ayured2011@gmail.com>
2025-01-31 20:00:55 +0530

    Update ElectricityReading.java

--- src/main/java/uk/tw/energy/domain/ElectricityReading.java
+++ src/main/java/uk/tw/energy/domain/ElectricityReading.java
@@ -3,9 +3,6 @@ package uk.tw.energy.domain;
 import java.math.BigDecimal;
 import java.time.Instant;
 
-/**
- * @param reading kW
- */
 
 // Encapsulation and Information Hiding approach - Getter and setter methods in models
 public record ElectricityReading(Instant time, BigDecimal reading) {}
